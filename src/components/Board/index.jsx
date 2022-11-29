d42e08a0bc2f7acf4e2c5b11409642dec13c254a
Newton Duarte <newton_@hotmail.com>
2022-11-29 10:13:54 -0300

    feat: implement move card and pass props via context do children

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
+import produce from 'immer';
+
+import BoardContext from './BoardContext';
+
 import { List } from '../List';
+import { loadLists } from '../../services/api';
 
 import * as S from './style';
 
-import { loadLists } from '../../services/api';
+
+const data = loadLists();
 
 export function Board() {
-  const lists = loadLists();
+  const [lists, setLists] = useState(data);
+
+  function moveCard(fromList, toList, from, to) {
+    setLists(produce(lists, (draft) => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }));
+  }
 
   return (
-    <S.Container>
-      {lists.map((list) => (
-        <List key={list.title} data={list} />
-      ))}
-    </S.Container>
+    <BoardContext.Provider value={{ lists, moveCard }}>
+      <S.Container>
+        {lists.map((list, index) => (
+          <List key={list.title} index={index} data={list} />
+        ))}
+      </S.Container>
+    </BoardContext.Provider>
   )
 }
